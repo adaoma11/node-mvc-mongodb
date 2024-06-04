85d7ab6c3d45b4a758a1b5e17179d77ed9ea6f12
Adão Araujo <adaoma11@hotmail.com>
2024-06-04 15:31:33 -0300

    feat: add show all products feature on /products route

--- controllers/ProductController.js
+++ controllers/ProductController.js
@@ -1,8 +1,10 @@
 const Product = require("../models/Product");
 
 module.exports = class ProductController {
-    static showProducts(req, res) {
-        res.render("products/all");
+    static async showProducts(req, res) {
+        const products = await Product.getProducts();
+
+        res.render("products/all", { products });
     }
 
     static createProduct(req, res) {
@@ -11,12 +13,13 @@ module.exports = class ProductController {
 
     static async createProductPost(req, res) {
         const name = req.body.name;
+        const image = req.body.image;
         const price = req.body.price;
         const description = req.body.description;
 
-        const product = new Product(name, price, description);
+        const product = new Product(name, image, price, description);
 
-        product.save();
+        await product.save();
 
         res.redirect("/products");
     }
